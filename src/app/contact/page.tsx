d525dbef7d5ffd0018efa4e76976fcf5a213268b
Your Namecls <junejowaseem0086@gmail.com>
2024-12-08 21:23:46 +0500

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <>
+      <div className="bg-[url('/images/contact.png')] bg-cover bg-center h-auto md:h-[400px] overflow-hidden ">
+        </div>
+      </>
+    );
+  }
+  
+  export default About;
+  
---
+++ src/app/shop/page.tsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <>
+      <div className="bg-[url('/images/shop.png')] bg-cover bg-center h-auto md:h-[400px] overflow-hidden ">
+        </div>
+      </>
+    );
+  }
+  
+  export default About;
+  
