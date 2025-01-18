cd6769486ab7c70199c8d7b7363c2dd40c9e50c2
Your Namecls <junejowaseem0086@gmail.com>
2025-01-18 11:39:39 +0500

    update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Blogs from "@/components/Blog";
 import Hero from "@/components/Hero";
 import Top2 from "@/components/HeroOne";
 import Insta from "@/components/insta";
-import ProductShop from "@/components/shop";
 import TopPicks from "@/components/Toppicks";
 
 function HomePage() {
@@ -11,7 +10,6 @@ function HomePage() {
         <>
         <Hero />
         <Top2 />
-        <ProductShop />
         <TopPicks />
         <Top4 />
         <Blogs />
